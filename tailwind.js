67c244255210b258c642c68209cadd6192476bae
Fred Gray <fred@fredgray.me>
2019-02-02 11:50:31 -0600

    More Changes

--- tailwind.js
+++ tailwind.js
@@ -45,6 +45,7 @@ let defaultConfig = require('tailwindcss/defaultConfig')()
 
 let colors = {
   'transparent': 'transparent',
+  'efx': '#A6012F',
 
   'black': '#22292f',
   'grey-darkest': '#3d4852',
